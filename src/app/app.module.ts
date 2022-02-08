a5c3fdcca1ea43a676d3c15f8e3e4304f089d1b2
Guada <guadalupe.alonso@gmail.com>
2022-02-08 06:55:11 -0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MiComponenteComponent } from './mi-componente/mi-componente.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiComponenteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mi-componente/mi-componente.component.ts
@@ -0,0 +1,60 @@
+import { ToDo } from './ToDo';
+import { Component, OnInit } from '@angular/core';
+import * as data from '../../../src/tareas.json';
+
+@Component({
+  selector: 'mi-componente',
+  templateUrl: './mi-componente.component.html',
+  styleUrls: ['./mi-componente.component.scss']
+})
+export class MiComponenteComponent implements OnInit {
+
+	public edad: number;
+	public nombres: Array<string>;
+  public tareas: Array<ToDo> = [];
+  public newTodo: string;
+
+  constructor() {
+	this.edad=18;
+	this.nombres = ["Guada", "Juan"];
+  // this.tareas = (data as any);
+  // console.log(this.tareas);
+  // this.tareas = new ToDo(this.http.get('tareas.json'));
+  
+	}
+
+
+
+  ngOnInit(): void {
+	  console.log("compoente cargado");
+
+  }
+  
+  aumentarEdad(){
+	  this.edad ++;
+  }
+  disminuirEdad(){
+	  this.edad --;
+  }
+
+  agregar(){
+    // this.tareas[] = new ToDo();
+    if (this.newTodo){
+      let td = new ToDo();
+      td.name=this.newTodo;
+      td.isCompleted=true;
+      this.tareas.push(td);
+      this.newTodo ="";
+    }else{
+      alert("vacio");
+    }
+  }
+
+  eliminar(id:number){
+    this.tareas[id].isCompleted = !this.tareas[id].isCompleted;
+  }
+  delete(id:number){
+    this.tareas = this.tareas.filter((v,l)=> l != id);
+  }
+
+}
